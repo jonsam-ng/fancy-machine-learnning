086d9ff70d6cd98bd6441f9217cd90d841d8e50e
wuqingshan <wuqingshan@xylink.com>
2022-04-09 13:33:29 +0800

    doc:chore: 🛠  update deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,8 @@ if [ -z "$GITHUB_TOKEN" ]; then
 else
   msg='来自github actions的自动部署'
   githubUrl=https://jonsam-ng:${GITHUB_TOKEN}@github.com/jonsam-ng/fancy-machine-learnning.git
-  git config --global user.name "jonsam"
-  git config --global user.email "jonsam.ng@foxmail.com"
+  git config --global user.name "wuqingshan"
+  git config --global user.email "wuqingshan@xylink.com"
 fi
 git init
 git add -A
@@ -27,7 +27,6 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 # if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
